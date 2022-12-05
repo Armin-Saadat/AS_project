c44721ce64bce097e14a8cc7f702a0999cc49173
Armin Saadat Boroujeni <arminsdt@rcl-nv1.ece.ubc.ca>
2022-12-04 16:11:43 -0800

    edit to run with simple r2plus1d

--- AS_thesis/get_config.py
+++ AS_thesis/get_config.py
@@ -25,11 +25,11 @@ def get_config():
     config['temp'] = 0.1
 
     # Hyperparameters for models.
-    config['model'] = "ava_r2plus1d_18"  # r2plus1d_18/x3d/resnet50/slowfast/tvn/ava_r2plus1d_18
+    config['model'] = "r2plus1d_18"  # r2plus1d_18/x3d/resnet50/slowfast/tvn/ava_r2plus1d_18
     config['pretrained'] = False
     config['restore'] = True
     config['loss_type'] = 'cross_entropy'  # cross_entropy/evidential/laplace_cdf/SupCon/SimCLR
-    config['use_ava'] = True
+    config['use_ava'] = False
 
     # Hyperparameters for training.
     config['batch_size'] = 6
